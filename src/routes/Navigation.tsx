e9038c7e3839e2832708e355b544cd566d1feb2b
Germán Gómez <gomezger.a9@gmail.com>
2024-01-28 14:04:59 -0300

    finished section routes

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,64 +1,52 @@
+import { Suspense } from "react";
 import {
 	BrowserRouter as Router,
 	Switch,
-	Route,
 	NavLink,
+	Route,
+	Redirect,
 } from "react-router-dom";
 
 import logo from "../logo.svg";
-import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
+import { routes } from "./routes";
 
 export const Navigation = () => {
 	return (
-		<Router>
-			<div className="main-layout">
-				<nav>
-					<img src={logo} alt="React Logo" />
-					<ul>
-						<li>
-							<NavLink
-								to="/lazy1"
-								activeClassName="nav-active"
-								exact
-							>
-								Lazy 1
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to="/lazy2"
-								activeClassName="nav-active"
-								exact
-							>
-								Lazy 2
-							</NavLink>
-						</li>
-						<li>
-							<NavLink
-								to="/lazy3"
-								activeClassName="nav-active"
-								exact
-							>
-								Lazy 3
-							</NavLink>
-						</li>
-					</ul>
-				</nav>
+		<Suspense fallback={<span> Loading ...</span>}>
+			<Router>
+				<div className="main-layout">
+					<nav>
+						<img src={logo} alt="React Logo" />
+						<ul>
+							{routes.map(({ to, name }) => (
+								<li key={to}>
+									<NavLink
+										to={to}
+										activeClassName="nav-active"
+										exact
+									>
+										{name}
+									</NavLink>
+								</li>
+							))}
+						</ul>
+					</nav>
 
-				{/* A <Switch> looks through its children <Route>s and
+					{/* A <Switch> looks through its children <Routes> and
             renders the first one that matches the current URL. */}
-				<Switch>
-					<Route path="/lazy3">
-						<LazyPage3 />
-					</Route>
-					<Route path="/lazy2">
-						<LazyPage2 />
-					</Route>
-					<Route path="/lazy1">
-						<LazyPage1 />
-					</Route>
-				</Switch>
-			</div>
-		</Router>
+					<Switch>
+						{routes.map((route) => (
+							<Route key={route.to} path={route.to}>
+								<route.Component />
+							</Route>
+						))}
+						<Route
+							path="/"
+							render={() => <Redirect to={routes[0].to} />}
+						/>
+					</Switch>
+				</div>
+			</Router>
+		</Suspense>
 	);
 };
